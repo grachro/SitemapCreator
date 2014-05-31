33a670991bd16e08922a46c6ef62c57bb62f67ae
grachro <grachro@gmail.com>
2014-05-31 17:05:14 +0900

    簡単な排他制御を追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,6 +16,41 @@ import com.grachro.sitemap.SiteLoader;
 
 public class Application extends Controller {
 
+	private static ApplicationSiteLoader appLoader = new ApplicationSiteLoader();
+
+	public static class ApplicationSiteLoader {
+		private boolean loading = false;
+		private SiteLoader loader = new SiteLoader();
+
+		public synchronized void load(int deep, String url) {
+			loading = true;
+			this.loader = new SiteLoader();
+			this.loader.load(deep, url);
+
+			File saveFile = getSavefile();
+			this.loader.save(saveFile);
+			loading = false;
+		}
+
+		public synchronized File getSavefile() {
+			File tempDir = new File("temp");
+			if (!tempDir.exists()) {
+				tempDir.mkdirs();
+			}
+
+			return new File(tempDir, "save.tsv");
+		}
+
+		public boolean isLoading() {
+			System.out.println("##########################");
+			System.out.println("##########################");
+			System.out.println("isLoading():" + this.loading);
+			System.out.println("##########################");
+			System.out.println("##########################");
+			return this.loading;
+		}
+	}
+
 	public static class InputForm {
 		public String url;
 		public int deep;
@@ -27,32 +62,51 @@ public class Application extends Controller {
 		return ok(index.render("Your new application is ready.", new Form(InputForm.class), EMPTY_LIST));
 	}
 
-	public static Result send() {
+	public static Result load() {
 		Form<InputForm> f = form(InputForm.class).bindFromRequest();
 		if (!f.hasErrors()) {
+			if (appLoader.isLoading()) {
+				return pleasWaitResutl();
+			}
 
-			InputForm data = f.get();
+			final InputForm data = f.get();
 			String msg = "target URL is  " + data.url;
 
-			SiteLoader loader = new SiteLoader();
-			loader.load(data.deep, data.url);
-			File saveFile = save(loader);
-			List<LoadedSite> resultUrls = loader.getResult();
+			Runnable r = new Runnable() {
+				@Override
+				public void run() {
+					appLoader.load(data.deep, data.url);
+				};
+			};
+			Thread t = new Thread(r);
+			t.start();
 
-			return ok(saveFile);
+			return ok(index.render(msg, f, EMPTY_LIST));
 		} else {
 			return badRequest(index.render("ERROR", form(InputForm.class), EMPTY_LIST));
 		}
 	}
 
-	private static File save(SiteLoader loader) {
-		File tempDir = new File("temp");
-		if (!tempDir.exists()) {
-			tempDir.mkdirs();
+	public static Result downloadTsv() {
+		if (appLoader.isLoading()) {
+			return pleasWaitResutl();
 		}
+		return ok(appLoader.getSavefile());
 
-		File saveFile = new File(tempDir, "save.tsv");
-		loader.save(saveFile);
-		return saveFile;
 	}
+
+	public static Result downloadSitemap() {
+		if (appLoader.isLoading()) {
+			return pleasWaitResutl();
+		}
+		return ok(appLoader.getSavefile());
+
+	}
+
+	private static Result pleasWaitResutl() {
+		String msg = "loading now. please wait.";
+		Form<InputForm> f = form(InputForm.class).bindFromRequest();
+		return ok(index.render(msg, f, EMPTY_LIST));
+	}
+
 }
